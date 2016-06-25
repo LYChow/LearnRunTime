9c46a2f628fb0ddd0e638f29dbb9bd0135f9554a
LY'S MacBook Air <zhouluyao@meicai.cn>
2016-06-25 19:17:54 +0800

    增加unrecognizeSelector时的消息转发功能 objc运行时三次拯救程序崩溃的机会

---
+++ LearnRunTime/MessageForward.h
@@ -0,0 +1,13 @@
+//
+//  MessageForward.h
+//  LearnRunTime
+//
+//  Created by LY'S MacBook Air on 6/25/16.
+//  Copyright © 2016 LY'S MacBook Air. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageForward : NSObject
+
+@end
---
+++ LearnRunTime/MessageForward.m
@@ -0,0 +1,13 @@
+//
+//  MessageForward.m
+//  LearnRunTime
+//
+//  Created by LY'S MacBook Air on 6/25/16.
+//  Copyright © 2016 LY'S MacBook Air. All rights reserved.
+//
+
+#import "MessageForward.h"
+
+@implementation MessageForward
+
+@end
